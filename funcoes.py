1b076705ce73e59574f570a7f0f45a23cfe7e5ec
carolinaam3 <carolinaam3@al.insper.edu.br>
2025-04-29 16:05:11 -0300

    guardar dados 5

--- funcoes.py
+++ funcoes.py
@@ -8,7 +8,8 @@ def rolar_dados(n):
 
 def guardar_dado(dados_rolados, dados_no_estoque, indice):
     dados_no_estoque.append(dados_rolados[indice])
-    dados_rolados2 = dados_rolados.remove(dados_rolados[indice])
-    lista= [dados_rolados2,dados_no_estoque]
+    del dados_rolados[indice]
+    
+    lista= [dados_rolados,dados_no_estoque]
     
     return lista 
