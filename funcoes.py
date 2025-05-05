29950d413a45467487c2d3897f02fa014068d511
carolinaam3 <carolinaam3@al.insper.edu.br>
2025-05-05 14:49:27 -0300

    função programa

--- funcoes.py
+++ funcoes.py
@@ -97,3 +97,20 @@ def faz_jogada (lista,categoria,dic):
     elif categoria in dic['regra_avancada']:
         dic['regra_avancada'][categoria] = calcula_pontos_regra_avancada(lista)[categoria]
     return dic
+
+def imprime_cartela(cartela):
+    print("Cartela de Pontos:")
+    print("-"*25)    
+    for i in range(1, 7):
+        filler = " " * (15 - len(str(i)))
+        if cartela['regra_simples'][i] != -1:
+            print(f"| {i}: {filler}| {cartela['regra_simples'][i]:02} |")
+        else:
+            print(f"| {i}: {filler}|    |")
+    for i in cartela['regra_avancada'].keys():
+        filler = " " * (15 - len(str(i)))
+        if cartela['regra_avancada'][i] != -1:
+            print(f"| {i}: {filler}| {cartela['regra_avancada'][i]:02} |")
+        else:
+            print(f"| {i}: {filler}|    |")
+    print("-"*25)
---
+++ programa.py
@@ -0,0 +1,17 @@
+import funcoes as f
+for i in range(12):
+    print('Cartela de Pontos:
+-------------------------
+| 1:               |    |
+| 2:               |    |
+| 3:               |    |
+| 4:               |    |
+| 5:               |    |
+| 6:               |    |
+| sem_combinacao:  |    |
+| quadra:          |    |
+| full_house:      |    |
+| sequencia_baixa: |    |
+| sequencia_alta:  |    |
+| cinco_iguais:    |    |
+-------------------------')
